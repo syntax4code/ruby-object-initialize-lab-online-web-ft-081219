3a07f44d8bf508a49234a57eb8903c66f72ffcee
Warren Loy Harper II <warrenharper2@att.net>
2019-08-20 21:52:28 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,24 +1,19 @@
 class Dog
-  def initialize(name)
-    @name = name
-    @breed = breed
+  def initialize (dogs_name, dogs_breed = "Mutt")
+    @name = dogs_name
+    @breed = dogs_breed
   end
- 
-  def name=(name)
-    @name = name
+
+  def name=(dogs_name, dogs_breed = "Mutt")
+    @name = dogs_name
+    @breed = dogs_breed
   end
- 
+
   def name
     @name
   end
-end
-
- def initialize(breed)
-  def breed=(breed)
-    @breed = breed
-  end
- 
   def breed
     @breed
-  end
-end
+  end 
+end 
+  
