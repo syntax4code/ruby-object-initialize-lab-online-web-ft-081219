b4ea3f9bc99fdcfde864eaa1987a42f29cff48c5
Warren Loy Harper II <warrenharper2@att.net>
2019-08-20 21:51:12 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,24 @@
+class Dog
+  def initialize(name)
+    @name = name
+    @breed = breed
+  end
+ 
+  def name=(name)
+    @name = name
+  end
+ 
+  def name
+    @name
+  end
+end
+
+ def initialize(breed)
+  def breed=(breed)
+    @breed = breed
+  end
+ 
+  def breed
+    @breed
+  end
+end
